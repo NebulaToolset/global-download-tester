f6dc4cd0b088b96dbc2cb1e3de77d8bb0a97d745
HoshinoTouko <oranly@hotmail.com>
2020-03-27 10:11:25 +0800

    Update Watcher.py

--- Core/Watcher.py
+++ Core/Watcher.py
@@ -59,7 +59,6 @@ class Watcher:
                 block_num, '%.2f%%' % (100 * self.traffic / total_size)]
         )))
         time_str = time_to_string(_current)
-        sys.stdout.flush()
         print('\r%s: %s' % (time_str, line), end='')
         # Log data
         with open(self.log_fp, 'a') as fi:
